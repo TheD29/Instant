3f0ec5ea641651511b18952634f4d6e1ad6ab95b
TheD29 <dmberezovskyi@gmail.com>
2018-07-04 16:21:55 +0300

    test

--- src/test/java/myprojects/automation/assignment5/tests/PlaceOrderTest.java
+++ src/test/java/myprojects/automation/assignment5/tests/PlaceOrderTest.java
@@ -22,30 +22,30 @@ public class PlaceOrderTest extends BaseTest {
         actions.setPhoneNumber();
         actions.WeatherMessageBody();
         actions.setFullName();
-        actions.setDayOfBirth();
-        /*man or women*/
-        actions.setGender();
-        /*Work or no*/
-        actions.getWorkType();//true no work/ false work
-        if (actions.getWorkType() == true) {
-            actions.setWorkExpirience();
-            actions.setMonthlyIncome();
-            actions.setNoConfirmationMonthlyIncome();
-            actions.setUploadLaterLink();
-            actions.setDownPayment();
-            actions.getAviableCarList();
-            actions.viewCar();
-            actions.setDeliveryOptions();
-            actions.orderSignature();
-            System.out.println(Data.getCapCost() + "capcost");
-            System.out.println(Data.getLeaseRate() + "leaserate");
-            System.out.println(Data.getTaxes() + "taxes");
-            System.out.println(Data.getTotalLeasePMT() + "total");
-//            actions.setCardParams();
-        } else {
-
-            actions.setDownPayment();
-        }
+//        actions.setDayOfBirth();
+//        /*man or women*/
+//        actions.setGender();
+//        /*Work or no*/
+//        actions.getWorkType();//true no work/ false work
+//        if (actions.getWorkType() == true) {
+//            actions.setWorkExpirience();
+//            actions.setMonthlyIncome();
+//            actions.setNoConfirmationMonthlyIncome();
+//            actions.setUploadLaterLink();
+//            actions.setDownPayment();
+//            actions.getAviableCarList();
+//            actions.viewCar();
+//            actions.setDeliveryOptions();
+//            actions.orderSignature();
+//            System.out.println(Data.getCapCost() + "capcost");
+//            System.out.println(Data.getLeaseRate() + "leaserate");
+//            System.out.println(Data.getTaxes() + "taxes");
+//            System.out.println(Data.getTotalLeasePMT() + "total");
+////            actions.setCardParams();
+//        } else {
+//
+//            actions.setDownPayment();
+//        }
 
 
     }
