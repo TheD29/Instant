196f38baf30ec7b6f810c6ef46db2afb4ed0c615
TheD29 <dmberezovskyi@gmail.com>
2018-07-04 13:30:15 +0300

    options

--- src/test/java/myprojects/automation/assignment5/utils/DriverFactory.java
+++ src/test/java/myprojects/automation/assignment5/utils/DriverFactory.java
@@ -64,6 +64,7 @@ public class DriverFactory {
                 options.addArguments("--disable-gpu"); // applicable to windows os only
                 options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
                 options.addArguments("--no-sandbox"); // Bypass OS security model
+                options.addArguments("--headless"); // Bypass OS security model
                 System.setProperty(
                         "webdriver.chrome.driver",
                         ("chromedriver"));
