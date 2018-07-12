04742403aa50017b0c88ed601febfef69ecba29f
TheD29 <dmberezovskyi@gmail.com>
2018-07-12 20:35:32 +0300

    add Answer checking

--- src/test/java/myprojects/automation/assignment5/AnswerActions.java
+++ src/test/java/myprojects/automation/assignment5/AnswerActions.java
@@ -1,11 +1,14 @@
 package myprojects.automation.assignment5;
 
+import myprojects.automation.assignment5.model.Answers;
 import myprojects.automation.assignment5.utils.logging.CustomReporter;
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class AnswerActions {
@@ -13,6 +16,9 @@ public class AnswerActions {
     WebDriverWait wait;
     JavascriptExecutor executor;
     Actions actions;
+
+    By buttonAnswer = By.xpath("//*[@class=\"profile-wrapper\"]/button[1]");
+    By titleText = By.xpath("//p[.=\"Answers\"]");
 
     public AnswerActions(WebDriver driver) {
         this.driver = driver;
@@ -27,15 +33,15 @@ public class AnswerActions {
             driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
             WebElement buttonYes = driver.findElement(By.xpath("//*[@class=\"working-types\"]/button[1]"));
             wait.until(ExpectedConditions.visibilityOf(buttonYes));
+            Answers.workingType = driver
+                    .findElement(By.xpath("//*[@class=\"working-types\"]/button[1]"))
+                    .getText();
             JavascriptExecutor executor = (JavascriptExecutor) driver;
             executor.executeScript("arguments[0].click();", buttonYes);
             CustomReporter.log("Step Set WorkType is Passed");
             return true;
 
         } catch (NoSuchElementException ex) {
-            WebElement buttonNo = driver.findElement(By.xpath("//*[@class=\"working-types\"]/button[2]"));
-            JavascriptExecutor executor = (JavascriptExecutor) driver;
-            executor.executeScript("arguments[0].click();", buttonNo);
             return false;
         }
 
@@ -45,23 +51,40 @@ public class AnswerActions {
 
         try {
             driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-            WebElement buttonYes = driver.findElement(By.xpath("//*[@class=\"working-types\"]/button[1]"));
-            wait.until(ExpectedConditions.visibilityOf(buttonYes));
-            JavascriptExecutor executor = (JavascriptExecutor) driver;
-            executor.executeScript("arguments[0].click();", buttonYes);
-            CustomReporter.log("Step Set WorkType is Passed");
-            return true;
-
-        } catch (NoSuchElementException ex) {
             WebElement buttonNo = driver.findElement(By.xpath("//*[@class=\"working-types\"]/button[2]"));
+            Answers.workingType = driver
+                    .findElement(By.xpath("//*[@class=\"working-types\"]/button[2]"))
+                    .getText();
             JavascriptExecutor executor = (JavascriptExecutor) driver;
             executor.executeScript("arguments[0].click();", buttonNo);
+            CustomReporter.log("Step Set WorkType is Passed");
+            return true;
+        } catch (NoSuchElementException ex) {
             return false;
         }
 
     }
 
     public void getAnswersWhenClientIsntWork() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(buttonAnswer)).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(titleText));
+        List<WebElement> answers = driver.findElements(By.xpath("//*[@class=\"answer-item\"]/b"));
+
+
+        try {
+            driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+            for (int i = 0; i < answers.size() - 1; i++) {
+                if (answers.get(i).equals(0)) {
+                    Assert.assertEquals(answers.get(i).getText(), Answers.monthlyBudget);
+                    break;
+                }
+            }
+
+
+        } catch (NoSuchElementException e) {
+
+        }
 
     }
 
--- src/test/java/myprojects/automation/assignment5/tests/PlaceOrderTest.java
+++ src/test/java/myprojects/automation/assignment5/tests/PlaceOrderTest.java
@@ -1,6 +1,7 @@
 package myprojects.automation.assignment5.tests;
 
 import myprojects.automation.assignment5.BaseTest;
+import myprojects.automation.assignment5.model.Answers;
 import myprojects.automation.assignment5.utils.Properties;
 import org.openqa.selenium.NoSuchElementException;
 import org.testng.annotations.Test;
@@ -85,13 +86,13 @@ public class PlaceOrderTest extends BaseTest {
             actions.returnToPreviousPage();
             Thread.sleep(500);
             actions.profileDeleting();
-        }catch (NoSuchElementException e){
+        } catch (NoSuchElementException e) {
             actions.profileDeleting();
         }
     }
 
-    @Test(enabled = false, priority = 6)
-    public void checkAnswers_icla103() throws InterruptedException {
+    @Test(enabled = true, priority = 6)
+    public void checkClientAnswers_icla103() throws InterruptedException {
         driver.get(Properties.getBaseUrl());
         actions.getCarList();
         actions.getBudgetList();
@@ -102,7 +103,6 @@ public class PlaceOrderTest extends BaseTest {
         String url = driver.getCurrentUrl();
         if (url.equals("https://demo.instantcarloanapproval.ca/returned")) {
             actions.checkUser();
-            System.out.println("current");
             actions.getCarList();
             actions.getBudgetList();
             actions.useFor();
@@ -114,15 +114,25 @@ public class PlaceOrderTest extends BaseTest {
         /*man or women*/
         actions.setGender();
         /*Work or no*/
-        actions.getWorkType();//true no work/ false work
-        if (actions.getWorkType() == true) {
-            actions.setWorkExpirience();
-            actions.setMonthlyIncome();
-            actions.setNoConfirmationMonthlyIncome();
-            actions.setUploadLaterLink();
-            actions.setDownPayment();
-        }
-    }
+        answer.clientIsntWorking();
+        actions.setDownPayment();
+        actions.getAviableCarList();
+        actions.viewCar();
+        actions.setDeliveryOptions();
+        actions.orderSignatureRecivedPayment();
+        actions.setCardParams();
+        actions.returnToHomePage();
+        actions.openProfile();
+        answer.getAnswersWhenClientIsntWork();
+//        actions.recivedPayment();
 
+        System.out.println(
+                Answers.monthlyBudget + "\n" +
+                        Answers.whyDoYouNeedACar + "\n" +
+                        Answers.gender + "\n" +
+                        Answers.workingType + "\n" +
+                        Answers.workingExpirience + "\n" +
+                        Answers.downPayment + "\n");
+    }
 
 }
