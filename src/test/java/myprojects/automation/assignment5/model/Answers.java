b68826a0158f07e29a1077d1a6a2447450ecbc9c
TheD29 <dmberezovskyi@gmail.com>
2018-07-13 11:14:38 +0300

    N

--- src/test/java/myprojects/automation/assignment5/model/Answers.java
+++ src/test/java/myprojects/automation/assignment5/model/Answers.java
@@ -2,14 +2,14 @@ package myprojects.automation.assignment5.model;
 
 public class Answers {
 
-   public static String monthlyBudget;
-   public static String whyDoYouNeedACar;
-   public static String birthDay;
-   public static String gender;
-   public static String workingType;
-   public static String workingExpirience;
-   public static String monthlyIncome;
-   public static String downPayment;
-   public static String confirmIncome;
+    public static String monthlyBudget;
+    public static String whyDoYouNeedACar;
+    public final static String birthDay = "11.11.1988";
+    public static String gender;
+    public static String workingType;
+    public static String workingExpirience;
+    public static String monthlyIncome;
+    public static String downPayment;
+    public static String confirmIncome;
 
 }
